f0b80548ad71e5953b67b337a296bae3204c19f5
ruger.crackme <550279039@qq.com>
2021-03-01 15:28:57 +0800

    feat: add detail page

--- app/src/main/java/com/example/androiddevchallenge/ui/Cat.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/Cat.kt
@@ -1,19 +1,32 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge
 
-import android.content.Context
-import android.util.Log
-import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Text
@@ -24,53 +37,87 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androiddevchallenge.data.CatInfo
 
 @Composable
-fun CatListLayout(context: Context, catList: List<CatInfo>) {
+fun CatListLayout(catList: List<CatInfo>, onClick: (CatInfo)->Unit) {
     LazyColumn(
         contentPadding = PaddingValues(8.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         items(catList) {
-            CatItemLayout(context, cat = it,
+            CatItemLayout(cat = it,
                 Modifier
-                    .fillMaxWidth())
+                    .fillMaxWidth(), onClick)
         }
     }
 }
 
 @Composable
-fun CatItemLayout(context: Context, cat: CatInfo, modifier: Modifier = Modifier) {
+fun CatItemLayout(cat: CatInfo, modifier: Modifier = Modifier, onClick: (CatInfo)->Unit) {
     Card(modifier) {
         Row(verticalAlignment = Alignment.CenterVertically){
             Image(
                 painterResource(cat.picture),
-                "Picture of dog: ${cat.name}",
+                "Cat Name: ${cat.name}",
                 Modifier.size(100.dp),
                 contentScale = ContentScale.Crop
             )
-            Column(Modifier.padding(8.dp).width(150.dp)) {
+            Column(
+                Modifier
+                    .padding(8.dp)
+                    .width(180.dp)) {
                 Text("Name: ${cat.name}", fontWeight= FontWeight.Bold)
-                Text("Location: ${cat.location}", fontWeight= FontWeight.Bold)
-                Text("Gender: ${cat.gender}", fontWeight= FontWeight.Bold)
                 Text("Age: ${cat.age}", fontWeight= FontWeight.Bold)
-                Text("vaccineInjected: ${cat.vaccineInjected}", fontWeight= FontWeight.Bold)
+                Text("Gender: ${cat.gender}", fontWeight= FontWeight.Bold)
             }
-            Log.d("gzz", "cat:${cat} ${cat.hashCode()} ${cat.isAdopted}")
+            Button(onClick = {
+                onClick(cat)
+            }) {
+                Text(text = "Detail")
+            }
+        }
+    }
+}
+
+@Composable
+fun CatDetailInfo(cat: CatInfo, onClick: (Boolean?) -> Unit) {
+    Column(Modifier
+            .fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painterResource(id = cat.picture), "Cat Name: ${cat.name}",
+            Modifier.size(300.dp).padding(top = 30.dp),
+            contentScale = ContentScale.Crop)
+        Column(
+            Modifier
+                .padding(8.dp)
+                .width(200.dp)) {
+            Text("Name: ${cat.name}", fontWeight= FontWeight.Bold, fontSize = 20.sp)
+            Text("Location: ${cat.location}", fontWeight= FontWeight.Bold, fontSize = 20.sp)
+            Text("Gender: ${cat.gender}", fontWeight= FontWeight.Bold, fontSize = 20.sp)
+            Text("Age: ${cat.age}", fontWeight= FontWeight.Bold, fontSize = 20.sp)
+            Text("vaccineInjected: ${cat.vaccineInjected}", fontWeight= FontWeight.Bold, fontSize = 20.sp)
+        }
+        Row {
             Button(onClick = {
                 if (!cat.isAdopted) {
-                    Toast.makeText(context, "Cat ${cat.name} is adopted", Toast.LENGTH_SHORT).show()
                     cat.isAdopted = true
+                    onClick(true)
                 } else {
-                    Toast.makeText(context, "Cat ${cat.name} is unadopted", Toast.LENGTH_SHORT).show()
                     cat.isAdopted = false
-                }}){
-                    if (!cat.isAdopted) {
-                        Text(text = "ADOPT")
-                    } else {
-                        Text(text = "UNADOPT")
-                    }
+                    onClick(false)
+                }
+            }) {
+                if (!cat.isAdopted) {
+                    Text(text = "Adopt")
+                } else {
+                    Text(text = "Cancel")
+                }
+            }
+            Button(onClick = {
+                onClick(null)
+            }, Modifier.padding(start = 30.dp)) {
+                Text(text = "Back")
             }
         }
     }
--- app/src/main/java/com/example/androiddevchallenge/ui/MainLayout.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/MainLayout.kt
@@ -1,6 +1,22 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge
 
 import android.content.Context
+import android.widget.Toast
 import androidx.compose.material.Scaffold
 import androidx.compose.material.SnackbarHostState
 import androidx.compose.material.Text
@@ -26,6 +42,23 @@ fun MainLayout(context: Context){
         }
     ) {
         val viewModel: CatViewModel = viewModel()
-        CatListLayout(context, viewModel.catList)
+        if (viewModel.currentCat == null) {
+            CatListLayout(viewModel.catList) {
+                viewModel.currentCat = it
+            }
+        } else {
+            CatDetailInfo(viewModel.currentCat!!) {
+                isAdopted ->
+                if (isAdopted != null) {
+                    var currentCat = viewModel.currentCat!!
+                    if (isAdopted) {
+                        Toast.makeText(context, "Cat ${currentCat.name} is adopted", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(context, "Cat ${currentCat.name} adopt is cancel", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                viewModel.currentCat = null
+            }
+        }
     }
 }
