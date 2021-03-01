1ce258048124e647f407a004483cead840575239
ruger.crackme <550279039@qq.com>
2021-03-01 13:49:12 +0800

    fix: lint check

--- app/src/main/java/com/example/androiddevchallenge/ui/Cat.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/Cat.kt
@@ -4,12 +4,19 @@ import android.content.Context
 import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,8 +25,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.data.CatInfo
-import kotlinx.coroutines.coroutineScope
-
 
 @Composable
 fun CatListLayout(context: Context, catList: List<CatInfo>) {
--- app/src/main/java/com/example/androiddevchallenge/ui/MainLayout.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/MainLayout.kt
@@ -1,12 +1,14 @@
 package com.example.androiddevchallenge
 
 import android.content.Context
-import androidx.compose.material.*
+import androidx.compose.material.Scaffold
+import androidx.compose.material.SnackbarHostState
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.SnackbarHost
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.androiddevchallenge.data.CatViewModel
-import kotlinx.coroutines.launch
 
 @Composable
 fun MainLayout(context: Context){
